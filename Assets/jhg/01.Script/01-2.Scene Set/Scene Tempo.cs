36e405d1fad2d861399748c9869ac77fbf3ab0c9
rund0297 <rund0297@gmail.com>
2024-09-08 17:16:16 +0900

    JHG

--- Assets/jhg/01.Script/01-2.Scene Set/Scene Tempo.cs
+++ Assets/jhg/01.Script/01-2.Scene Set/Scene Tempo.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator; // 애니메이션 컨트롤러
+    public KeyCode key = KeyCode.LeftShift; // 실행할 키
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+
+    }
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
     }
 
+    private IEnumerator CloseAfterDelay()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) // 지정한 키가 눌리면
+        {
+            animator.SetTrigger("TitleAnim");// 애니메이션 실행
+            yield return new WaitForSeconds(0.5f);
+            gameObject.SetActive(false);
+            animator.ResetTrigger("TitleAnim");
+        }
+        
+    }
+    public void Start()
+    {
+       
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightShift))
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             SceneManager.LoadScene("KJH");
         }
     }
+
+
 }
--- Assets/jhg/01.Script/01-3.Animetion/animations.cs
+++ Assets/jhg/01.Script/01-3.Animetion/animations.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class animations : MonoBehaviour
 {
     public Animator animator; // 애니메이션 컨트롤러
-    public KeyCode key = KeyCode.Space; // 실행할 키
+    public KeyCode key = KeyCode.RightShift; // 실행할 키
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightShift)) // 지정한 키가 눌리면
         {
-            animator.SetTrigger("Run"); // 애니메이션 실행
+            animator.SetTrigger("TitleAnim"); // 애니메이션 실행
         }
     }
 }
