f5eb94f42f2f00d39a4003109d785d3023ad7a7d
lake04 <wjdghksrla0409@gmail.com>
2024-09-08 17:26:44 +0900

    kkk

--- Assets/KJH/boss.cs
+++ Assets/KJH/boss.cs
@@ -4,128 +4,52 @@ using UnityEngine;
 
 public class boss : MonoBehaviour
 {
-    
-    public  int hp = 52;
- 
-    //소환
+    public float hp = 10f;
     [SerializeField]
     GameObject enemy;
-    [SerializeField]
-    GameObject bullet;
-
     Animator anim;
-    //스폰 위치
     public  Transform spawn;
-    public  Transform bulltSp;
 
-    //소환 패턴 쿨타임
-   // private float coolTime = 5f;
-    private float coolTime = 1.5f;
-    private float pattern1Time = 1f;
-    private bool isAk;
 
-    //카메라
-    private float shakeTime;
-    private float shakeIntensity;
-    private float camerTime = 0.2f;
-
-    public ParticleSystem da;
-
-    bool hasSpawned = false;
-
-    
-
-  
-
+    private float coolTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        da.Stop();
-        /*InvokeRepeating("bulltSpawn", 2f, 3f);*/
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
-        SpawnEnemyAtHP(hp);
-        if (coolTime<=0)
-        { 
-               
-        }
-        else if(coolTime >=0)
-        {
-             
-            coolTime -= Time.deltaTime;
-        }
-      
-
-        if (hp<=0)
-        {
-            Destroy(this.gameObject);
-        }
+        attack();
+        StartCoroutine(pattern1());
     }
 
- 
-    //소환 패턴 쿨타임
+
     private IEnumerator pattern1()
     {
+
+       
         yield return new WaitForSeconds(1f);
-        coolTime = 1.5f;
+     
     }
-    void SpawnEnemyAtHP(int hp)
+
+    private void attack()
     {
-        if (hp % 10 == 0 && hp < 50 && hp >= 10&& !hasSpawned)  // 90부터 10까지 10단위로 체크
+        if (hp >0 && coolTime <= 0)
         {
+            
+            hp--;
+
             Instantiate(enemy, spawn.position, spawn.rotation);
-            hasSpawned = true;
+            coolTime = 2f;
+
         }
-         else if(hp%10 != 0)
+        if (coolTime >= 0)
         {
-            hasSpawned=false;
-        }
-    }
-
-    //피격
-    public void TakeDamage(int damage)
-    {
-        hp = hp - damage;
-        da.Play();
-
-        OnShakeCamera(0.1f, 1.0f);
-
-    }
-
-    public void OnShakeCamera(float shakeTime = 1.0f, float shakeIntensity = 0.1f)
-    {
-        this.shakeTime = shakeTime;
-        this.shakeIntensity = shakeIntensity;
-
-        StopCoroutine("shakebyPosition");
-        StartCoroutine("shakebyPosition");
-    }
-    private IEnumerator shakebyPosition()
-    {
-        Vector3 startPosition = transform.position;
-
-        while (shakeTime > 0.0f)
-        {
-            transform.position = startPosition + Random.insideUnitSphere * shakeIntensity;
-
-            shakeTime -= Time.deltaTime;
-
-            yield return null;
+      
+            coolTime = coolTime - Time.deltaTime;
         }
 
-        transform.position = startPosition;
-    }
-
-   
-
-    private void bulltSpawn()
-    {
-        Instantiate(bullet, bulltSp.transform.position, Quaternion.identity);
     }
 }
--- Assets/KJH/teleport.cs
+++ Assets/KJH/teleport.cs
@@ -6,50 +6,24 @@ public class teleport : MonoBehaviour
 {
 
     public GameObject player;
-    //µµÂø ÁöÁ¡
     public GameObject end;
-    public GameObject ui;
-
-    public bool isTeleport;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isTeleport)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-               
-                player.transform.position = end.transform.position;
-            }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            isTeleport = true;
-           
-
-        }
         
-       
-
-        if (collision.gameObject.tag == "Player")
-        {
-            ui.SetActive(true);
-        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        isTeleport = false;
-        ui.SetActive(false);
+        if(collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            player.transform.position = end.transform.position;
+        }
     }
 }
